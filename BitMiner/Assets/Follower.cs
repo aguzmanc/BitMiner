aa9e23fb5f8ddbae44571c3905f8cc32e1641b93
TheFrankemon <francomontiel@hotmail.com>
2018-01-27 12:57:56 -0400

    Added 8-directional way player and movement. Added camera that follows player with smoothness

---
+++ BitMiner/Assets/Follower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+
+	public Transform Player;
+	public float Smoothness = 0.125f;
+	public Vector3 PlayerOffset;
+
+	void FixedUpdate () {
+		Vector3 desiredPosition = Player.position + PlayerOffset;
+		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, Smoothness);
+		transform.position = smoothedPosition;
+
+		transform.LookAt (Player);
+	}
+}
