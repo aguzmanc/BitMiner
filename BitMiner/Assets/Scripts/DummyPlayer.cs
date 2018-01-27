efbb3b3a1341cc3de4c3595c66a658fbdeb6631f
arn <arn@ancestralgods.com>
2018-01-27 10:33:31 -0400

    Dummy Floor + Terminals Test

---
+++ BitMiner/Assets/Scripts/DummyPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyPlayer : MonoBehaviour 
+{
+	public float Velocity = 2f;
+
+	public float hh;
+	void Update () 
+	{
+		hh = Input.GetAxis("Horizontal");
+		float vv = Input.GetAxis("Vertical");
+
+		Vector3 direction = new Vector3(hh, 0, vv).normalized;
+		transform.rotation = Quaternion.LookRotation(direction);
+	}
+}
