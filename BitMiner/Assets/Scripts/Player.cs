5e526ef96699256201349d3e4c2cdecef9e81b3f
TheFrankemon <francomontiel@hotmail.com>
2018-01-27 16:54:10 -0400

    Modified input mapping for XYAB buttons. Refactorized movement

--- BitMiner/Assets/Scripts/Player.cs
+++ BitMiner/Assets/Scripts/Player.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 	[Range(10,100)]
 	public int MovementSpeed = 40;
-	public Transform ReferenceVector;
 
 	float _horizontalAxis;
 	float _verticalAxis;
 
 	void Update () {
+		handleMovement ();
+		handleButtons ();
+	}
+
+	void handleMovement() {
 		_horizontalAxis = Input.GetAxis ("Horizontal");
 		_verticalAxis = Input.GetAxis ("Vertical");
 
 		Vector3 movement = new Vector3 (0.01f * MovementSpeed * _horizontalAxis, 0, 0.01f * MovementSpeed * _verticalAxis);
-		transform.Translate (ReferenceVector.TransformDirection(movement));
+		transform.Translate(movement, Space.World);
+	}
+
+	void handleButtons () {
+		if (Input.GetButtonDown ("Cross")) { //X Button on PS4 - A Button on XBox
+			Debug.Log ("X / A pressed");
+		} else if (Input.GetButtonDown ("Square")) { //□ Button on PS4 - X Button on XBox
+			Debug.Log ("□ / X pressed");
+		} else if (Input.GetButtonDown ("Circle")) { //○ Button on PS4 - B Button on XBox
+			Debug.Log ("○ / B pressed");
+		} else if (Input.GetButtonDown ("Triangle")) { //△ Button on PS4 - Y Button on XBox
+			Debug.Log ("△ / Y pressed");
+		}
 	}
 }
