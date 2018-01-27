77069a8834c4ed71ff0bdeffc8a764b9991963c4
arn <arn@ancestralgods.com>
2018-01-27 11:06:48 -0400

    Basic Movement

--- BitMiner/Assets/Scripts/DummyPlayer.cs
+++ BitMiner/Assets/Scripts/DummyPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DummyPlayer : MonoBehaviour 
 {
 	public float Velocity = 2f;
+	public Transform ReferenceTransformMovement;
 
 	public float hh;
 	void Update () 
@@ -13,6 +14,11 @@ public class DummyPlayer : MonoBehaviour
 		float vv = Input.GetAxis("Vertical");
 
 		Vector3 direction = new Vector3(hh, 0, vv).normalized;
+		transform.TransformDirection(*)
+		//direction = ReferenceTransformMovement.TransformDirection(direction).normalized;
 		transform.rotation = Quaternion.LookRotation(direction);
+
+		transform.Translate(
+			new Vector3(0,0,direction.magnitude * Velocity * Time.deltaTime), Space.Self);
 	}
 }
