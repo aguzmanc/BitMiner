d0d730a7cf406b62e76aef6e835177fd4dcece2e
Allan Leon <aleon.107@outlook.com>
2018-01-27 11:27:50 -0400

    Created patrol and detector scripts

---
+++ BitMiner/Assets/Scripts/Detector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Detector : MonoBehaviour {
+	public float Range = 5f;
+	[Range(0, 60)]
+	public float DegreesOfVision = 30;
+	[Range(0.1f, 2)]
+	public float Accuracy = 1;
+	public string TagToDetect = "Player";
+
+	PatrolAgent _patrolAgent;
+
+	// Use this for initialization
+	void Start () {
+		_patrolAgent = GetComponent<PatrolAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool foundTarget = false;
+		Vector3 targetPosition = Vector3.forward;
+		for (float i = -DegreesOfVision; i < DegreesOfVision; i += Accuracy) {
+			Debug.Log (i);
+			Ray ray = new Ray (transform.position, Quaternion.Euler (0, i, 0) * transform.forward);
+			RaycastHit hit;
+			Color rayColor = Color.blue;
+			if (Physics.Raycast (ray, out hit, Range)) {
+				if (hit.collider.tag == TagToDetect) {
+					foundTarget = true;
+					targetPosition = hit.collider.transform.position;
+					rayColor = Color.green;
+					break;
+				}
+			}
+			Debug.DrawRay (ray.origin, ray.direction * Range, rayColor);
+		}
+		if (foundTarget && _patrolAgent) {
+			_patrolAgent.GoToSpecificPoint (targetPosition);
+		} else {
+			_patrolAgent.KeepPatrolling ();
+		}
+	}
+}
---
+++ BitMiner/Assets/Scripts/PatrolAgent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolAgent : MonoBehaviour {
+	public Transform[] Points;
+	public float Speed = 5f;
+	public GameObject PointsContainer;
+	int _destPointIndex;
+	NavMeshAgent _agent;
+	bool _isPatrolling;
+
+	// Use this for initialization
+	void Start () {
+		if (PointsContainer) {
+			Transform[] obtainedPoints = PointsContainer.GetComponentsInChildren<Transform> ();
+			Points = new Transform[obtainedPoints.Length - 1];
+			for (int i = 1; i < obtainedPoints.Length; i++) {
+				Points [i - 1] = obtainedPoints [i];
+			}
+		}
+		transform.position = Points [0].position;
+		_agent = GetComponent<NavMeshAgent> ();
+		_agent.autoBraking = false;
+		_agent.speed = Speed;
+		GoToNextPoint ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_agent.speed = Speed;
+		if (_isPatrolling) {
+			if (!_agent.pathPending && _agent.remainingDistance < 0.5f) {
+				GoToNextPoint ();
+			}
+		}
+	}
+
+	void GoToNextPoint() {
+		_destPointIndex = (_destPointIndex + 1) % Points.Length;
+		_agent.destination = Points[_destPointIndex].position;
+	}
+
+	public void GoToSpecificPoint(Vector3 point) {
+		_agent.destination = point;
+		_isPatrolling = false;
+	}
+
+	public void KeepPatrolling() {
+		_agent.destination = Points[_destPointIndex].position;
+		_isPatrolling = true;
+	}
+}
